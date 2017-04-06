0ae463b61b5d93b35fc53eb0da8e87b6b0321622
Mark Banks <mark@abu.local>
2017-04-06 11:26:12 +1000

    Updated for FM15

--- SimplePlugin.cpp
+++ SimplePlugin.cpp
@@ -3,7 +3,7 @@
  SimplePlugin.cpp
  SimplePlugin
 
- Copyright 2010-2013 Mark Banks. All rights reserved.
+ Copyright 2010-2017 Mark Banks. All rights reserved.
  License: see the accompyanying file License.txt
  
  http://banks.id.au/filemaker/plugins/simpleplugin/
@@ -11,46 +11,47 @@
  */
 
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wall"
-
-#include "FMWrapper/FMXBinaryData.h"
-#include "FMWrapper/FMXCalcEngine.h"
-#include "FMWrapper/FMXClient.h"
-#include "FMWrapper/FMXData.h"
-#include "FMWrapper/FMXDateTime.h"
-#include "FMWrapper/FMXExtern.h"
-#include "FMWrapper/FMXFixPt.h"
-#include "FMWrapper/FMXText.h"
-#include "FMWrapper/FMXTextStyle.h"
-#include "FMWrapper/FMXTypes.h"
-
-#pragma clang diagnostic pop
+#define FMX_USE_UNIQUE_PTR 1
 
 
-#include <string>
+#include <FMWrapper/FMXBinaryData.h>
+#include <FMWrapper/FMXCalcEngine.h>
+#include <FMWrapper/FMXClient.h>
+#include <FMWrapper/FMXData.h>
+#include <FMWrapper/FMXDateTime.h>
+#include <FMWrapper/FMXExtern.h>
+#include <FMWrapper/FMXFixPt.h>
+#include <FMWrapper/FMXText.h>
+#include <FMWrapper/FMXTextStyle.h>
+#include <FMWrapper/FMXTypes.h>
+
+
 #include <iostream>
+#include <string>
 
 
 #pragma mark -
 #pragma mark Prototypes
 #pragma mark -
 
-fmx::QuadCharAutoPtr PluginID ( void );
-fmx::TextAutoPtr PluginPrefix ( void );
-fmx::TextAutoPtr PluginOptionsString ( void );
+const fmx::ptrtype Init ( FMX_ExternCallPtr /* pb */ );
+void Shutdown ( void );
+
+const fmx::QuadCharUniquePtr PluginID ( void );
+const fmx::TextUniquePtr PluginPrefix ( void );
+const fmx::TextUniquePtr PluginOptionsString ( void );
 
 void GetString ( FMX_ExternCallPtr pb );
-fmx::errcode RegisterFunction ( std::string prototype, fmx::ExtPluginType function, bool evaluate_on_server = true );
+const fmx::errcode RegisterFunction ( const std::string prototype, const fmx::ExtPluginType function, const std::string description = "" );
 void UnregisterFunctions ( void );
-fmx::TextAutoPtr FunctionName ( fmx::TextAutoPtr& signature );
-void NumberOfParameters ( fmx::TextAutoPtr& signature, short& required, short& optional );
+const fmx::TextUniquePtr FunctionName ( const fmx::TextUniquePtr& signature );
+void NumberOfParameters ( const fmx::TextUniquePtr& signature, short& required, short& optional );
 
 
 // pragma mark generates a warning C4068 in Visual Studio so that warning is disabled in VS
 
 #pragma mark -
-#pragma mark Enums, defines &  Globals
+#pragma mark Enums, Defines & Globals
 #pragma mark -
 
 enum {
@@ -102,19 +103,19 @@ short g_next_function;	// used to keep track of the funciton id number
  
  **************************************************************************** */
 
+fmx::errcode Limit ( short /* function_id */, const fmx::ExprEnv& /* environment */, const fmx::DataVect& parameters, fmx::Data& reply );
 
-FMX_PROC(fmx::errcode) Limit ( short function_id, const fmx::ExprEnv& environment, const fmx::DataVect& parameters, fmx::Data& reply )
+fmx::errcode Limit ( short /* function_id */, const fmx::ExprEnv& /* environment */, const fmx::DataVect& parameters, fmx::Data& reply )
 {
-#pragma usnused ( function_id, environment )
-	
+
 	fmx::errcode error = kSPNoError;
 	
 	// extract the first paramter to the function
 	
-	fmx::FixPtAutoPtr number;
+	fmx::FixPtUniquePtr number;
 	number->AssignFixPt ( parameters.AtAsNumber ( kSPFirstParameter ) );
 	
-	fmx::FixPtAutoPtr lower_limit;
+	fmx::FixPtUniquePtr lower_limit;
 	
 	// only get the lower and upper limits if they exist (otherwise we crash)
 	
@@ -124,20 +125,20 @@ FMX_PROC(fmx::errcode) Limit ( short function_id, const fmx::ExprEnv& environmen
 		lower_limit->AssignInt ( 0 );
 	}
 
-	fmx::FixPtAutoPtr upper_limit;
+	fmx::FixPtUniquePtr upper_limit;
 	if ( parameters.Size() == 3 ) {
 		upper_limit->AssignFixPt ( parameters.AtAsNumber ( kSPThirdParameter ) );
 	} else {
-		long precision = number->GetPrecision();
+		fmx::int32 precision = number->GetPrecision();
 		upper_limit->AssignInt ( precision * 10 );
 	}
 	
 	// apply the limits
 	
 	if ( *number < *lower_limit ) {
-		number = lower_limit;
+		number->AssignFixPt ( *lower_limit );
 	} else if ( *number > *upper_limit ) {
-		number = upper_limit;
+		number->AssignFixPt ( *upper_limit );
 	}
 	
 	// send the result of the calculation back to FileMaker
@@ -164,26 +165,27 @@ FMX_PROC(fmx::errcode) Limit ( short function_id, const fmx::ExprEnv& environmen
  perform any setup and register functions
  */
 
-fmx::errcode Init ( FMX_ExternCallPtr pb ) 
+const fmx::ptrtype Init ( FMX_ExternCallPtr /* pb */ )
 {
-#pragma unused ( pb )
-	
+
 	fmx::errcode error = kSPNoError;
-	fmx::errcode enable = kCurrentExtnVersion; // kDoNotEnable to prevent the plugin loading
+	fmx::ptrtype enable = kCurrentExtnVersion; // kDoNotEnable to prevent the plugin loading
 
 	
 	// perform any initialisation and set-up globals
 	
-	
-	
+
 	/*
 	 register plug-in functions
 	 
-	 functions must always be registered in the same order (to avoid breaking existing 
-	 calculation in FM).
+	 functions must always be registered in the same order (to avoid breaking existing calculation in FM).
 	 */
 
-	error = RegisterFunction ( "Limit ( number {; lowerLimit ; upperLimit } )", Limit );
+	error = RegisterFunction ( "Limit ( number {; lowerLimit ; upperLimit } )", Limit, "This function is Limit ed.!" );
+
+	if ( kSPNoError != error ) {
+		enable = (fmx::ptrtype)kDoNotEnable;
+	}
 
 	return enable;
 	
@@ -246,7 +248,7 @@ void FMX_ENTRYPT FMExternCallProc ( FMX_ExternCallPtr pb )
 
 void GetString ( FMX_ExternCallPtr pb )
 {
-	fmx::TextAutoPtr string;
+	fmx::TextUniquePtr string;
 	
 	switch ( pb->parm1 )
 	{
@@ -264,7 +266,7 @@ void GetString ( FMX_ExternCallPtr pb )
 			
 	}
 	
-	string->GetUnicode ( (fmx::unichar*)(pb->result), 0, fmx::Text::kSize_End );
+	string->GetUnicode ( (fmx::unichar16*)(pb->result), 0, fmx::Text::kSize_End );
 	
 } // GetString
 
@@ -277,41 +279,42 @@ void GetString ( FMX_ExternCallPtr pb )
  1. the external function signature as it should appear in the calcuation dialogs but
  without the prefix i.e. Explode ( timer ) rather than Bomb_Explode ( timer )
  2.	the plug-in function to call when the function is used in FileMaker
- 3. (optional) should the function be available via FileMaker Server? default: true
+ 3. (optional) a description of the function ... default: ""
  */
 
-fmx::errcode RegisterFunction ( std::string prototype, fmx::ExtPluginType function, bool evaluate_on_server )
+const fmx::errcode RegisterFunction ( const std::string prototype, const fmx::ExtPluginType function, const std::string description )
 {
-	fmx::TextAutoPtr underscore;
+	fmx::TextUniquePtr underscore;
 	underscore->Assign ( "_" );
 
-	fmx::TextAutoPtr signature;
-	signature->Assign ( prototype.c_str() );
-	signature->InsertText ( *PluginPrefix(), 0 );
-	signature->InsertText ( *underscore, kSPPrefixLength );
+	fmx::TextUniquePtr function_protoype;
+	function_protoype->Assign ( prototype.c_str() );
+	function_protoype->InsertText ( *PluginPrefix(), 0 );
+	function_protoype->InsertText ( *underscore, kSPPrefixLength );
 	
-	fmx::TextAutoPtr name;
-	name->SetText ( *FunctionName ( signature ) );
+	fmx::TextUniquePtr function_description;
+	function_description->Assign ( description.c_str() );
+
+	fmx::TextUniquePtr name;
+	name->SetText ( *FunctionName ( function_protoype ) );
 	
 	short required_parameters = 0;
 	short optional_rarameters = 0;
-	NumberOfParameters ( signature, required_parameters, optional_rarameters );
+	NumberOfParameters ( function_protoype, required_parameters, optional_rarameters );
 	
-	fmx::ulong function_flags = fmx::ExprEnv::kDisplayInAllDialogs;
-	if ( evaluate_on_server ) {
-		function_flags |= fmx::ExprEnv::kMayEvaluateOnServer;
-	}
-	
-	fmx::errcode error = fmx::ExprEnv::RegisterExternalFunction ( *PluginID(),
-																 g_next_function, 
-																 *name, 
-																 *signature, 
-																 required_parameters, 
-																 required_parameters + optional_rarameters, 
-																 function_flags, 
-																 function 
-																 );
-	
+	const fmx::uint32 function_flags = fmx::ExprEnv::kDisplayInAllDialogs;
+
+	const fmx::errcode error = fmx::ExprEnv::RegisterExternalFunctionEx ( *PluginID(),
+																		 g_next_function,
+																		 *name,
+																		 *function_protoype,
+																		 *function_description,
+																		 required_parameters,
+																		 required_parameters + optional_rarameters,
+																		 function_flags,
+																		 function
+																		 );
+
 	if ( error != kSPNoError ) {
 		std::cerr << "Error registering: " << prototype << "! Error #: " << error << std::endl;
 	}
@@ -335,13 +338,13 @@ void UnregisterFunctions ( void )
 
 // automaticlly generate the PluginID from the prefix
 
-fmx::QuadCharAutoPtr PluginID ( void )
+const fmx::QuadCharUniquePtr PluginID ( void )
 {
-	fmx::TextAutoPtr prefix;
+	fmx::TextUniquePtr prefix;
 	prefix->SetText ( *PluginPrefix() );
 	char buffer[kSPPluginIDLength];
 	prefix->GetBytes ( buffer, kSPPluginIDLength );
-	fmx::QuadCharAutoPtr id ( buffer[0], buffer[1], buffer[2], buffer[3] );
+	fmx::QuadCharUniquePtr id ( buffer[0], buffer[1], buffer[2], buffer[3] );
 
 	return id;
 }
@@ -349,9 +352,9 @@ fmx::QuadCharAutoPtr PluginID ( void )
 
 // use the defined prefix if it exists otherwise use the first four characters of the name
 
-fmx::TextAutoPtr PluginPrefix ( void )
+const fmx::TextUniquePtr PluginPrefix ( void )
 {
-	fmx::TextAutoPtr prefix;
+	fmx::TextUniquePtr prefix;
 
 #ifdef PLUGIN_PREFIX
 	prefix->Assign ( PLUGIN_PREFIX );
@@ -366,9 +369,9 @@ fmx::TextAutoPtr PluginPrefix ( void )
 
 // use the options string defined above otherwise turn everything on
 
-fmx::TextAutoPtr PluginOptionsString ( void )
+const fmx::TextUniquePtr PluginOptionsString ( void )
 {
-	fmx::TextAutoPtr optionsString;
+	fmx::TextUniquePtr optionsString;
 	
 #ifdef PLUGIN_OPTIONS_STRING
 	optionsString->Assign ( PLUGIN_OPTIONS_STRING );
@@ -384,30 +387,30 @@ fmx::TextAutoPtr PluginOptionsString ( void )
 
 // extract the function name from a function signature/prototype
 
-fmx::TextAutoPtr FunctionName ( fmx::TextAutoPtr& signature )
+const fmx::TextUniquePtr FunctionName ( const fmx::TextUniquePtr& signature )
 {
 	
-	fmx::TextAutoPtr separator;
+	fmx::TextUniquePtr separator;
 	separator->Assign ( "(" );
 	
-	fmx::ulong parameters_start = signature->Find ( *separator, 0 );
+	fmx::uint32 parameters_start = signature->Find ( *separator, 0 );
 	if ( parameters_start == fmx::Text::kSize_Invalid ) {
 		parameters_start = fmx::Text::kSize_End;
 	} else {
 		
 		// there may or may not be spaces between the function name and the bracket
 		
-		fmx::TextAutoPtr space;
+		fmx::TextUniquePtr space;
 		space->Assign ( " " );
 		
-		fmx::ulong last = parameters_start - 1;
+		fmx::uint32 last = parameters_start - 1;
 		while ( signature->Find ( *space, last ) == last ) {
 			--last;
 		}
 		parameters_start = last + 1;
 	}
 	
-	fmx::TextAutoPtr name;
+	fmx::TextUniquePtr name;
 	name->SetText ( *signature, 0, parameters_start );
 	
 	return name;
@@ -417,35 +420,35 @@ fmx::TextAutoPtr FunctionName ( fmx::TextAutoPtr& signature )
 
 // calculate the number of required and optional parameters from a function signature/prototye
 
-void NumberOfParameters ( fmx::TextAutoPtr& signature, short& required, short& optional )
+void NumberOfParameters ( const fmx::TextUniquePtr& signature, short& required, short& optional )
 {
 	required = 0;
 	optional = 0;
 	
-	fmx::TextAutoPtr separator;
+	fmx::TextUniquePtr separator;
 	separator->Assign ( "(" );
 	
-	fmx::ulong parameters_start = signature->Find ( *separator, 0 );
+	const fmx::uint32 parameters_start = signature->Find ( *separator, 0 );
 	if ( parameters_start == fmx::Text::kSize_Invalid ) {
 		return;
 	}
 	
 	// we have parameters
 	
-	fmx::TextAutoPtr semi_colon;
+	fmx::TextUniquePtr semi_colon;
 	semi_colon->Assign ( ";" );
 	
-	fmx::TextAutoPtr curly_bracket;
+	fmx::TextUniquePtr curly_bracket;
 	curly_bracket->Assign ( "{" );	
 	
 	bool has_optional_parameters = false;
-	fmx::ulong next = parameters_start;
+	fmx::uint32 next = parameters_start;
 	
 	while ( next != fmx::Text::kSize_Invalid ) {
 		
 		++next;
-		fmx::ulong next_semi_colon = signature->Find ( *semi_colon, next );
-		fmx::ulong next_curly_bracket = signature->Find ( *curly_bracket, next );
+		const fmx::uint32 next_semi_colon = signature->Find ( *semi_colon, next );
+		const fmx::uint32 next_curly_bracket = signature->Find ( *curly_bracket, next );
 		
 		if ( next_curly_bracket < next_semi_colon && has_optional_parameters == false ) {
 			
@@ -453,7 +456,7 @@ void NumberOfParameters ( fmx::TextAutoPtr& signature, short& required, short& o
 			++required;
 			has_optional_parameters = true;
 			
-			fmx::TextAutoPtr elipsis;
+			fmx::TextUniquePtr elipsis;
 			elipsis->Assign ( "…" );
 			
 			if ( signature->Find ( *elipsis, next_curly_bracket + 1 ) != fmx::Text::kSize_Invalid ) {
@@ -461,7 +464,7 @@ void NumberOfParameters ( fmx::TextAutoPtr& signature, short& required, short& o
 				next = fmx::Text::kSize_Invalid;
 			} else {
 				
-				fmx::TextAutoPtr faux_elipsis;
+				fmx::TextUniquePtr faux_elipsis;
 				faux_elipsis->Assign ( "..." );
 				
 				if ( signature->Find ( *faux_elipsis, next_curly_bracket + 1 ) != fmx::Text::kSize_Invalid ) {
